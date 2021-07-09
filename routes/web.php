eb02b37cac5f1ae65287217c0dcca31d000db905
awadh-hassan <awadhhassan77@gmail.com>
2021-07-09 20:31:48 +0300

    profile

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,9 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('profile',  function(){
+    return view('profile.index', array('user' => Auth::user()));
+});
 
 require __DIR__.'/auth.php';
