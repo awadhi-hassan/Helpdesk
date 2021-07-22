a351092c88b4c48aaf60c231f119fd4af49faefe
awadh-hassan <awadhhassan77@gmail.com>
2021-07-22 21:08:25 +0300

    Added and styled the tickets page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,33 +11,33 @@
             <div class="overflow-hidden shadow-sm sm:rounded-lg">
                 <div>
                     <div class="p-6 bg-white flex" style="justify-content: space-around;" id="dashboard">
-                        <a href="{{ route('ticket') }}">
+                        <a style="color: black; text-decoration: none" href="{{ route('ticket') }}">
                             <div class="card mt-3" id="ticket" style="padding: 20px;">
                                 <div class="flex ml-10">
                                     <img src="{{ asset('ticket.jpeg') }}" style="height: 50px; width: auto;" alt="">
-                                    <h2 class="mt-3"><b>Create Ticket</b></h2>
+                                    <h5 class="mt-3 ml-2"><b>Create Ticket</b></h5>
                                 </div>
                                 <div class="container">
                                     <small>Raise an issue you cant fix yourself.</small>
                                 </div>
                             </div>
                         </a>
-                        <a href="">
+                        <a style="color: black; text-decoration: none" href="">
                             <div class="card mt-3" id="faqs" style="padding: 20px;">
                                 <div class="flex ml-10">
                                     <img src="{{ asset('faq.png') }}" style="height: 50px; width: auto;" alt="">
-                                    <h2 class="mt-3"><b>FAQs</b></h2>
+                                    <h5 class="mt-3 ml-2"><b>FAQs</b></h5>
                                 </div>
                                 <div class="container">
                                     <small>Go through the frequently asked questions.</small>
                                 </div>
                             </div>
                         </a>
-                        <a href="">
+                        <a style="color: black; text-decoration: none" href="">
                             <div class="card mt-3" id="contactus" style="padding: 20px;">
                                 <div class="flex ml-10">
                                     <img src="{{ asset('contact.png') }}" style="height: 50px; width: auto;" alt="">
-                                    <h2 class="mt-3"><b>Contact US</b></h2>
+                                    <h5 class="mt-3 ml-2"><b>Contact US</b></h5>
                                 </div>
                                 <div class="container">
                                     <small>Information on how to reach us (phone and mail)</small>
@@ -46,22 +46,22 @@
                         </a>
                     </div>
                     <div class="p-6 bg-white flex" style="justify-content: space-around;" id="dashboard_lower">
-                        <a href="">                        
+                        <a style="color: black; text-decoration: none" href="">                        
                             <div class="card mt-3" id="feedback" style="padding: 20px;">
                                 <div class="flex ml-10">
                                     <img src="{{ asset('feedback.png') }}" style="height: 50px; width: auto;" alt="">
-                                    <h2 class="mt-3"><b>Feedback</b></h2>
+                                    <h5 class="mt-3 ml-2"><b>Feedback</b></h5>
                                 </div>
                                 <div class="container">
                                     <small>Give feedback on your XP at KSG</small>
                                 </div>
                             </div>
                         </a>
-                        <a href="">                            
+                        <a style="color: black; text-decoration: none" href="">                            
                             <div class="card mt-3" id="forum" style="padding: 20px;">
                                 <div class="flex ml-10">
                                     <img src="{{ asset('forum.png') }}" style="height: 50px; width: auto;" alt="">
-                                    <h2 class="mt-3"><b>Forum</b></h2>
+                                    <h5 class="mt-3 ml-2"><b>Forum</b></h5>
                                 </div>
                                 <div class="container">
                                     <small class="ml-5">Share your XP with other users</small>
--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -3,47 +3,58 @@
         <strong>Create Your ticket</strong>
     </x-slot>
 
-    <div class="py-5">
-        <form action="" method="POST">
-            <div>
-                <label for="text">Title</label><br>
-                <input type="text" name="title" placeholder="title"/>
-            </div>
+    <div class="py-3">
+        <div class="container d-flex justify-content-center">
+            <div class="col-7">
 
-            <div>
-                <label for="designation">Designation</label><br>
-                <select name="designation" id="designation">
-                    <option value="ict">ICT</option>
-                    <option value="recas">Recas</option>
-                    <option value="hr">Human Resource</option>
-                    <option value="catering">Catering</option>
-                    <option value="hk">House Keeping</option>
-                    <option value="aintenance">Maintenance</option>
-                    <option value="accomodation">Accomodation</option>
-                    <option value="accounting">Accounting</option>
-                    <option value="procurement">Procurement</option>
-                </select>
-            </div>
+                <form action="" method="POST">
+                    @csrf_token
+                    <div class="form-group my-2">
+                        <label for="text"><b>Title</b></label><br>
+                        <input class="form-control" type="text" name="title" :value="old('title')" placeholder="title"/>
+                    </div>
+        
+                    <div class="form-group my-2">
+                        <label for="designation"><b>Designation</b></label><br>
+                        <select class="form-control" name="designation" :value="old('designation')" id="designation">
+                            <option value="ict">ICT</option>
+                            <option value="recas">Recas</option>
+                            <option value="hr">Human Resource</option>
+                            <option value="catering">Catering</option>
+                            <option value="hk">House Keeping</option>
+                            <option value="aintenance">Maintenance</option>
+                            <option value="accomodation">Accomodation</option>
+                            <option value="accounting">Accounting</option>
+                            <option value="procurement">Procurement</option>
+                        </select>
+                    </div>
+        
+                    <div class="form-group my-2">
+                        <label for="priority"><b>Priority</b></label><br>
+                        <select class="form-control" name="priority" id="priority" :value="old('priority')">
+                            <option value="high">High</option>
+                            <option value="medium">Medium</option>
+                            <option value="low">Low</option>
+                        </select>
+                    </div>
+        
+                    <div class="form-group my-2">
+                        <label for="dec"><b>Description</b></label><br>
+                        <textarea class="form-control" name="desc" :value="old('desc')" id="desc" cols="35" rows="5" placeholder="please describe the problem you are facing..."></textarea>
+                    </div>
+        
+                    <div class="form-group my-2">
+                        <label for="pic"><b>Sample Picture</b></label><br>
+                        <input class="form-control-file" type="file" name="pic" id="pic">
+                    </div>
 
-            <div>
-                <label for="priority">Priority</label><br>
-                <select name="priority" id="priority">
-                    <option value="high">High</option>
-                    <option value="medium">Medium</option>
-                    <option value="low">Low</option>
-                </select>
-            </div>
+                    <div class="form-group my-2 d-flex justify-content-end">
+                        <button class="btn btn-primary" type="submit">Create Ticket</button>
+                    </div>
 
-            <div>
-                <label for="dec">Description</label><br>
-                <textarea name="desc" id="desc" cols="35" rows="5" placeholder="please describe the problem you are facing..."></textarea>
-            </div>
+                </form>
 
-            <div>
-                <label for="pic">Sample Picture</label><br>
-                <input type="file" name="pic" id="pic">
-            </div>
-
-        </form>
+            </div>    
+        </div>
     </div>
 </x-app-layout>
